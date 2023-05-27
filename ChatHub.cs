01209787e92631fa77067d29562cec06356ff975
frailin02 <108911322+frailin02@users.noreply.github.com>
2023-05-26 21:34:00 -0300

    Add files via upload

---
+++ ChatHub.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatRoom
+{
+    public class ChatHub : Hub 
+    {
+        public async Task SendMessage(string room, string user, string message)
+        {
+            //Enviamos un mensaje de forma asincrona a un grupo determinado
+            await Clients.Group(room.ToString()).SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async Task AddToGroup(string room)
+        {
+            //Añadimos a la persona a la sala
+            await Groups.AddToGroupAsync(Context.ConnectionId, room);
+            //await Clients.Group(room.ToString()).SendAsync("ShowWho", $"Conectando {Context.ConnectionId}");
+        }
+    }
+}
---
+++ Controllers/ChatController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatRoom.Controllers
+{
+    public class ChatController : Controller
+    {
+        public static Dictionary<int, string> Salas =
+            new Dictionary<int, string>(){
+            {1, "Sala Uno" },
+            {2, "Sala Dos" },
+            {3, "Sala Tres" }
+        };
+
+        public IActionResult ListaDeSalas()
+        {
+            return View(Salas);
+        }
+
+        public IActionResult Cuarto(int sala)
+        {
+            return View("Sala", sala);
+        }
+    }
+}
